b49b4eaa0f67d62d8c7dfcf8345c6d35323db7f6
GUL12290 <101751449+GUL12290@users.noreply.github.com>
2023-04-28 07:53:00 -0600

    Create datos_n_dias.py

---
+++ datos_n_dias.py
@@ -0,0 +1,45 @@
+import json
+import pandas as pd
+from sodapy import Socrata
+
+
+class Get_Data():
+    def __init__(self, df= pd.DataFrame()):
+        self.df= df
+    
+    def create_df(self):
+        
+        client = Socrata("data.lacity.org", None)
+        results = client.get("2nrs-mtv8", limit=800000)
+        results_df = pd.DataFrame.from_records(results)
+        results_df.to_csv(r'lapd.csv')
+        self.df = results_df
+    
+    
+    def create_df_n_days(self,n):
+        client = Socrata("data.lacity.org", None)
+        # filtro para traer los ultimos n días
+        date_filter = (datetime.utcnow() - timedelta(days=n)).strftime("%Y-%m-%dT%H:%M:%S.%f")
+        results = client.get("2nrs-mtv8", where=f"date_rptd >= '{date_filter}'", limit=800000)
+        results_df = pd.DataFrame.from_records(results)
+        results_df.to_csv(r'lapd_'+str(n)+'_days.csv')
+        self.df = results_df       
+
+        
+    def infer_dtypes(self):
+        ls_dates=["date_rptd", "date_occ"]
+        ls_numeric=["vict_age","lat", "lon"]
+        ls_strings=[x for x in self.df.columns if x not in ls_dates+ls_numeric]
+        for date in ls_dates:
+            self.df[date]= pd.to_datetime(self.df[date])
+        for num in ls_numeric:
+            self.df[num]= pd.to_numeric(self.df[num])
+        for s in ls_strings:
+            self.df[s].apply(lambda x: str(x).split(','))
+        self.df["time_occ"]=self.df["time_occ"].apply(lambda x: int(x[0:2])*60+int(x[2:]))
+
+
+Data= Get_Data()
+Data.create_df_n_days(20)
+Data.infer_dtypes()
+df= Data.df.copy()
